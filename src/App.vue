b543f354844f6dabf133dcbbdd744739021d8fb8
Jose Vega <jocvegar@gmail.com>
2020-11-14 20:33:49 -0600

    changes logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img id="image" alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Hola Jose Vega"/>
   </div>
 </template>
@@ -24,5 +24,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#image {
+  height: 20em;
 }
 </style>
